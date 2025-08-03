b47c8ec3737fd96048b88acafba16371d5b0c9c5
FrancescoSls97 <frankds1997@gmail.com>
2025-08-03 10:14:27 +0200

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+
 
 function App() {
 
 
   return (
     <>
-
+      <header>
+        <nav className="navbar navbar-expand-sm navbar-light bg-secondary p-3">
+          <div className="container-fluid">
+            <a href="#" className="navbar-brand">My Movies Site</a>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                <li className="nav-item"><a href="" className="nav-link">Movies</a></li>
+                <li className="nav-item"><a href="" className="nav-link">Contacts</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
     </>
   )
 }
